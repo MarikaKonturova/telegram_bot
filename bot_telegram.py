5f27cbf5ebe27f6e2948df6033cb2725b35e30e4
MarikaKonturova <mashakonturova@gmail.com>
2023-08-31 14:57:04 +0300

    add admin commands

--- bot_telegram.py
+++ bot_telegram.py
@@ -8,6 +8,7 @@ async def on_startup(_):
 
 
 client.register_handlers_client(dp)
+admin.register_handlers_client(dp)
 other.register_handlers_client(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
